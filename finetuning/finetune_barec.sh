b860adcea7c0936922ef74b8528e1cf0b11fb1fa
bensapirstein <ben.sapirstein@gmail.com>
2025-09-07 23:14:48 +0300

    evals and plots

--- finetuning/finetune_barec.sh
+++ finetuning/finetune_barec.sh
@@ -1,19 +1,19 @@
 # Optional wandb environment vars
-export WANDB_PROJECT="pixel-morphology-experiments"
+export WANDB_PROJECT="pixel-orthography-experiments"
 
 # Settings
 export FALLBACK_FONTS_DIR="data/fallback_fonts"  # let's say this is where we downloaded the fonts to
-export MODEL="Team-PIXEL/pixel-base" # also works with "bert-base-cased", "roberta-base", etc.
+export MODEL="Team-PIXEL/pixel-m4" # also works with "bert-base-cased", "roberta-base", etc.
 # export MODEL="bensapir/pixel-barec-pretrain" # also works with "bert-base-cased", "roberta-base", etc.
 # export REPO="pixel-base-finetune-sent"
 export REPO="pixel-base-finetune-d3tok-space-sent"
 export SEQ_LEN=256
-export BSZ=32
+export BSZ=64
 export GRAD_ACCUM=1
-export LR=2.5e-05
+export LR=5e-05
 export SEED=42
 export NUM_STEPS=3000
-export NUM_EPOCHS=5
+export NUM_EPOCHS=7
 export MODE="${MODE:-arabic-default}"  # default if not set
 
 export RUN_NAME="$(basename ${MODEL})-${MODE}-${SEQ_LEN}-${BSZ}-${GRAD_ACCUM}-${LR}-${NUM_EPOCHS}-${SEED}"
@@ -31,17 +31,17 @@ python scripts/training/run_readability.py \
   --per_device_train_batch_size=${BSZ} \
   --gradient_accumulation_steps=${GRAD_ACCUM} \
   --learning_rate=${LR} \
-  --warmup_steps=200 \
+  --warmup_steps=100 \
   --run_name="${RUN_NAME}" \
   --output_dir="runs/${RUN_NAME}" \
   --overwrite_cache \
   --text_renderer_name_or_path="configs/renderers/noto_renderer" \
   --logging_strategy=steps \
-  --logging_steps=200 \
+  --logging_steps=100 \
   --evaluation_strategy=steps \
-  --eval_steps=400 \
+  --eval_steps=200 \
   --save_strategy=steps \
-  --save_steps=400 \
+  --save_steps=200 \
   --save_total_limit=1 \
   --report_to=wandb \
   --log_predictions \
